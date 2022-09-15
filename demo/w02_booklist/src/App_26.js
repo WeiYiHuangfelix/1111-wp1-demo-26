a85f8316a40346b78194b2b26002981a90b2bea3
WeiYiHuangfelix <a0908882001@gmail.com>
2022-09-15 16:54:26 +0800

    W02-P5: copy W02 files to demo/w02_booklist

---
+++ demo/w02_booklist/src/App_26.js
@@ -0,0 +1,9 @@
+//import logo from './logo.svg';
+import './App_26.css';
+import Booklist_26 from './components/Booklist_26';
+
+const App_26 = () => {
+  return <Booklist_26 />;
+};
+
+export default App_26;
